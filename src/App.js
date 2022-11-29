1ce6666b94da57830d300b8b84a01a18390911ba
Joe DeSoto <modulusman@gmail.com>
2022-11-29 14:28:23 -0800

    Adds App.css file. Also brings in use effect and a fetch to call the movies data.
    Verified this is working by trying the API out with Spiderman, which
    is showing the results as expected.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import { useEffect, React } from "react";
 const API_KEY = "6b006179";
 const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
   return <h1>Hello from App.js</h1>;
 };
 
