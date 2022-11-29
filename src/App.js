c59b5ed141b160f6476fb099099b956bcaca7e1c
Joe DeSoto <modulusman@gmail.com>
2022-11-29 14:19:34 -0800

    Removes boilerplate code files from CRA and adds what we need for the project.
    Places (public) API key in project so we can use OMDB.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, React } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const API_KEY = "6b006179";
+const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
+const App = () => {
+  return <h1>Hello from App.js</h1>;
+};
 
 export default App;
